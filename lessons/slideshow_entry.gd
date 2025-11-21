8e0b647d0f36634c3522c602e7cab163fdcede20
jngdotcom <jaguo4@s.sfusd.edu>
2025-11-21 14:48:10 -0800

    m8l5

--- lessons/slideshow_entry.gd
+++ lessons/slideshow_entry.gd
@@ -1,1 +1,7 @@
-extends Node
+class_name SlideShowEntry extends Resource
+
+@export var character: Texture = preload("uid://copkt78vgp37i")
+@export var expression: Texture = preload("uid://d000xjtr4iqtk")
+
+
+@export_multiline var text:= ""
