daee1489d3a1193fcec5a7d378ae7f11049cb1c3
jngdotcom <jaguo4@s.sfusd.edu>
2025-12-12 10:06:29 -0800

    m8l6 FINAL FIX

--- lessons/dialogue.gd
+++ lessons/dialogue.gd
@@ -1,56 +1,12 @@
 extends Control
 
-var expressions := {
-	"happy": preload ("res://assets/emotion_happy.png"),
-	"regular": preload ("res://assets/emotion_regular.png"),
-	"sad": preload ("res://assets/emotion_sad.png"),
-}
-
-var bodies := {
-	"sophia": preload ("res://assets/sophia.png"),
-	"pink": preload ("res://assets/pink.png")
-}
 
 ## An array of dictionaries. Each dictionary has four properties:
 ## - expression: a [code]Texture[/code] containing an expression
 ## - text: a [code]String[/code] containing the text the character says
 ## - character: a [code]Texture[/code] representing the character, that we extract from the [code]bodies[/code] dictionary
 ## - choices: a [code]dictionary[/code] with [code]String[/code] keys and [code]int[/code] values
-var dialogue_items: Array[Dictionary] = [
-	{
-		"expression": expressions["regular"],
-		"text": "[wave]grr[/wave]\n how is it doing that.",
-		"character": bodies["sophia"],
-		"choices": {
-			"what": 2,
-			"do what": 1,
-		},
-	},
-	{
-		"expression": expressions["happy"],
-		"text": "its gone.",
-		"character": bodies["sophia"],
-		"choices": {
-			"lets get it back": 3,
-			"that sucks man...": 2,
-		},
-	},
-	{
-		"expression": expressions["sad"],
-		"text": "what the eff",
-		"character": bodies["pink"],
-		"choices": {
-			"huh": 0,
-			"yeah it does": 1,
-		},
-	},
-	{
-		"expression": expressions["happy"],
-		"text": "ok [wave] lets.get that dog. [/wave]",
-		"character": bodies["pink"],
-		"choices": {"Okay! (Quit)": - 1},
-	},
-]
+@export var dialogue_items: Array[DialogueItem] = []
 
 ## UI element that shows the texts
 @onready var rich_text_label: RichTextLabel = %RichTextLabel
@@ -75,10 +31,10 @@ func show_text(current_item_index: int) -> void:
 	# from the item, we extract the properties.
 	# We set the text to the rich text control
 	# And we set the appropriate expression texture
-	rich_text_label.text = current_item["text"]
-	expression.texture = current_item["expression"]
-	body.texture = current_item["character"]
-	create_buttons(current_item["choices"])
+	rich_text_label.text = current_item.text
+	expression.texture = current_item.expression
+	body.texture = current_item.character
+	create_buttons(current_item.choices)
 	# We set the initial visible ratio to the text to 0, so we can change it in the tween
 	rich_text_label.visible_ratio = 0.0
 	# We create a tween that will draw the text
@@ -115,23 +71,17 @@ func show_text(current_item_index: int) -> void:
 ## [param choices_data] A dictionary of [String] keys where each key represents
 ##                      a sentence that the player can select, and each [int] value
 ##                      represents a key for the next text item.
-func create_buttons(choices_data: Dictionary) -> void:
-	# We remove all previous buttons
+func create_buttons(choices_data: Array[DialogueChoice]) -> void:
 	for button in action_buttons_v_box_container.get_children():
 		button.queue_free()
-	# We loop over all the dictionary keys
-	for choice_text in choices_data:
+	for choice in choices_data:
 		var button := Button.new()
 		action_buttons_v_box_container.add_child(button)
-		button.text = choice_text
-		# We extract the target line index from the dictionary value
-		var target_line_idx: int = choices_data[choice_text]
-		if target_line_idx == - 1:
-			# If the target line index is -1, we want to quit
+		button.text = choice.text
+		if choice.is_quit == true:
 			button.pressed.connect(get_tree().quit)
 		else:
-			# Otherwise we bind the target line index to the show_text function
-			# and use that in the pressed signal's connection
+			var target_line_idx := choice.target_line_idx
 			button.pressed.connect(show_text.bind(target_line_idx))
 
 ## Animates the character when they start talking
